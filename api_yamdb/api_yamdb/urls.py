e145ad425d34f9c575d44a7146d0cdeba53fd05e
Владимир Беликов <Belikrastr@yandex.ru>
2022-11-16 00:15:07 +0300

    Авторизация и аутентификация(JWT)

--- api_yamdb/api_yamdb/urls.py
+++ api_yamdb/api_yamdb/urls.py
@@ -1,12 +1,17 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('auth/', include('django.contrib.auth.urls')),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
         name='redoc'
     ),
+    path('api/v1/', include('djoser.urls')),
+    # JWT-эндпоинты, для управления JWT-токенами:
+    path('api/v1/', include('djoser.urls.jwt')),
+
 ]
