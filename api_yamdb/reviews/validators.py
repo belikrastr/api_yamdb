bdcd4da51a3874f4114236ced63597238412bb7a
Владимир Беликов <Belikrastr@yandex.ru>
2022-11-24 14:05:15 +0300

    PEP8

--- api_yamdb/reviews/validators.py
+++ api_yamdb/reviews/validators.py
@@ -1,6 +1,7 @@
 from datetime import datetime as dt
 from django.core.exceptions import ValidationError
 
+
 def validate_year(val):
     current_year = dt.date.today().year
     if val > current_year:
